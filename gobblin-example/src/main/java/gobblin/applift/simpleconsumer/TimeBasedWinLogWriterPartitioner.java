ba1e6c65f211636f448ae0a7d15f3fcdfd67506a
Prashant Bhardwaj <prashant.bhardwaj@applift.com>
2016-07-01 02:34:12 +0530

    Increased default kafka fetch size from 1MB to 10MB

--- gobblin-example/src/main/java/gobblin/applift/simpleconsumer/TimeBasedWinLogWriterPartitioner.java
+++ gobblin-example/src/main/java/gobblin/applift/simpleconsumer/TimeBasedWinLogWriterPartitioner.java
@@ -1,9 +1,14 @@
 package gobblin.applift.simpleconsumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import gobblin.configuration.State;
 import gobblin.writer.partitioner.TimeBasedWriterPartitioner;
 
 public class TimeBasedWinLogWriterPartitioner extends TimeBasedWriterPartitioner<String>{
+  private static final Logger LOG = LoggerFactory.getLogger(TimeBasedWinLogWriterPartitioner.class);
+  
 	public TimeBasedWinLogWriterPartitioner(State state, int numBranches, int branchId) {
 		super(state, numBranches, branchId);
 	}
@@ -11,8 +16,13 @@ public class TimeBasedWinLogWriterPartitioner extends TimeBasedWriterPartitioner
 	@Override
 	public long getRecordTimestamp(String record) {
 		String[] columns = record.split("\\|");
-		Double unixTS = Double.valueOf(columns[38])*1000;
-		long timestampMS = unixTS.longValue();
+		long timestampMS = System.currentTimeMillis();
+		try {
+		  Double unixTS = Double.valueOf(columns[38])*1000;
+		  timestampMS = unixTS.longValue();
+    } catch (Exception e) {
+      LOG.error("Applift Faulty Record: "+record);
+    }
 		return timestampMS;
 	}
 }
