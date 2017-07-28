cce5ba2dbfac991dc2abd92c2cc2f3f3535bcdab
Prashant Bhardwaj <prashant.bhardwaj@applift.com>
2017-07-28 16:34:37 +0530

    1. Added new fields to KYT logs.

--- gobblin-example/src/main/java/gobblin/applift/simpleconsumer/ClickHouseRecord.java
+++ gobblin-example/src/main/java/gobblin/applift/simpleconsumer/ClickHouseRecord.java
@@ -1,199 +1,310 @@
 package gobblin.applift.simpleconsumer;
 
 public class ClickHouseRecord {
-  private String os;
-  private String country_id;
-  private String exchange_id;
-  private String publisher_id;
-  private String app_id;
-  private String bundle_id;
-  private String device_id;
-  private long timestamp;
-  private int age;
-  private String gender;
-  private int request;
-  private int bid;
-  private int imp;
-  private int click;
-  private int conversion;
-  /**
-   * @return the device_id
-   */
-  public String getDeviceId() {
-    return device_id;
-  }
-  /**
-   * @param device_id the device_id to set
-   */
-  public void setDeviceId(String device_id) {
-    this.device_id = device_id;
-  }
-  /**
-   * @return the os
-   */
-  public String getOs() {
-    return os;
-  }
-  /**
-   * @param os the os to set
-   */
-  public void setOs(String os) {
-    this.os = os;
-  }
-  /**
-   * @return the country_id
-   */
-  public String getCountryId() {
-    return country_id;
-  }
-  /**
-   * @param country_id the country_id to set
-   */
-  public void setCountryId(String country_id) {
-    this.country_id = country_id;
-  }
-  /**
-   * @return the exchange_id
-   */
-  public String getExchangeId() {
-    return exchange_id;
-  }
-  /**
-   * @param exchange_id the exchange_id to set
-   */
-  public void setExchangeId(String exchange_id) {
-    this.exchange_id = exchange_id;
-  }
-  /**
-   * @return the publisher_id
-   */
-  public String getPublisherId() {
-    return publisher_id;
-  }
-  /**
-   * @param publisher_id the publisher_id to set
-   */
-  public void setPublisherId(String publisher_id) {
-    this.publisher_id = publisher_id;
-  }
-  /**
-   * @return the app_id
-   */
-  public String getAppId() {
-    return app_id;
-  }
-  /**
-   * @param app_id the app_id to set
-   */
-  public void setAppId(String app_id) {
-    this.app_id = app_id;
-  }
-  /**
-   * @return the bundle_id
-   */
-  public String getBundleId() {
-    return bundle_id;
-  }
-  /**
-   * @param bundle_id the bundle_id to set
-   */
-  public void setBundleId(String bundle_id) {
-    this.bundle_id = bundle_id;
-  }
-  /**
-   * @return the timestamp
-   */
-  public long getTimestamp() {
-    return timestamp;
-  }
-  /**
-   * @param timestamp the timestamp to set
-   */
-  public void setTimestamp(long timestamp) {
-    this.timestamp = timestamp;
-  }
-  /**
-   * @return the age
-   */
-  public int getAge() {
-    return age;
-  }
-  /**
-   * @param age the age to set
-   */
-  public void setAge(int age) {
-    this.age = age;
-  }
-  /**
-   * @return the gender
-   */
-  public String getGender() {
-    return gender;
-  }
-  /**
-   * @param gender the gender to set
-   */
-  public void setGender(String gender) {
-    this.gender = gender;
-  }
-  /**
-   * @return the request
-   */
-  public int getRequest() {
-    return request;
-  }
-  /**
-   * @param request the request to set
-   */
-  public void setRequest(int request) {
-    this.request = request;
-  }
-  /**
-   * @return the bid
-   */
-  public int getBid() {
-    return bid;
-  }
-  /**
-   * @param bid the bid to set
-   */
-  public void setBid(int bid) {
-    this.bid = bid;
-  }
-  /**
-   * @return the imp
-   */
-  public int getImp() {
-    return imp;
-  }
-  /**
-   * @param imp the imp to set
-   */
-  public void setImp(int imp) {
-    this.imp = imp;
-  }
-  /**
-   * @return the click
-   */
-  public int getClick() {
-    return click;
-  }
-  /**
-   * @param click the click to set
-   */
-  public void setClick(int click) {
-    this.click = click;
-  }
-  /**
-   * @return the conversion
-   */
-  public int getConversion() {
-    return conversion;
-  }
-  /**
-   * @param conversion the conversion to set
-   */
-  public void setConversion(int conversion) {
-    this.conversion = conversion;
-  }
+	private int age = 0;
+	private String app_id = "";
+	private String app_name = "";
+	private double bid_floor = 0;
+	private String bundle_id = "";
+	private int carrier_id = 0;
+	private int connection_type = 0;
+	private int country_id = 0;
+	private String date = "";
+	private String device_id = "";
+	private int dnt = 0;
+	private int exchange_id = 0;
+	private int gender = 0;
+	private int has_bidfloor = 0;
+	private int has_deviceid = 0;
+	private int has_location = 0;
+	private int height = 0;
+	private int hour = 0;
+	private int impression_type = 0;
+	private int make_id = 0;
+	private int max_height = 0;
+	private int max_width = 0;
+	private int min_height = 0;
+	private int min_width = 0;
+	private int os = 0;
+	private int osv = 0;
+	private String publisher_id = "";
+	private String publisher_name = "";
+	private int session_depth = 0;
+	private long timestamp = 0;
+	private int traffic_type = 0;
+	private int video_height = 0;
+	private int video_width = 0;
+	private int width = 0;
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getApp_id() {
+		return app_id;
+	}
+
+	public String getApp_name() {
+		return app_name;
+	}
+
+	public double getBid_floor() {
+		return bid_floor;
+	}
+
+	public String getBundle_id() {
+		return bundle_id;
+	}
+
+	public int getCarrier_id() {
+		return carrier_id;
+	}
+
+	public int getConnection_type() {
+		return connection_type;
+	}
+
+	public int getCountry_id() {
+		return country_id;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getDevice_id() {
+		return device_id;
+	}
+
+	public int getDnt() {
+		return dnt;
+	}
+
+	public int getExchange_id() {
+		return exchange_id;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getHour() {
+		return hour;
+	}
+
+	public int getImpression_type() {
+		return impression_type;
+	}
+
+	public int getMake_id() {
+		return make_id;
+	}
+
+	public int getMax_height() {
+		return max_height;
+	}
+
+	public int getMax_width() {
+		return max_width;
+	}
+
+	public int getMin_height() {
+		return min_height;
+	}
+
+	public int getMin_width() {
+		return min_width;
+	}
+
+	public int getOs() {
+		return os;
+	}
+
+	public int getOsv() {
+		return osv;
+	}
+
+	public String getPublisher_id() {
+		return publisher_id;
+	}
+
+	public String getPublisher_name() {
+		return publisher_name;
+	}
+
+	public int getSession_depth() {
+		return session_depth;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public int getTraffic_type() {
+		return traffic_type;
+	}
+
+	public int getVideo_height() {
+		return video_height;
+	}
+
+	public int getVideo_width() {
+		return video_width;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int isHas_bidfloor() {
+		return has_bidfloor;
+	}
+
+	public int isHas_deviceid() {
+		return has_deviceid;
+	}
+
+	public int isHas_location() {
+		return has_location;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setApp_id(String app_id) {
+		this.app_id = app_id;
+	}
+
+	public void setApp_name(String app_name) {
+		this.app_name = app_name;
+	}
+
+	public void setBid_floor(double bid_floor) {
+		this.bid_floor = bid_floor;
+	}
+
+	public void setBundle_id(String bundle_id) {
+		this.bundle_id = bundle_id;
+	}
+
+	public void setCarrier_id(int carrier_id) {
+		this.carrier_id = carrier_id;
+	}
+
+	public void setConnection_type(int connection_type) {
+		this.connection_type = connection_type;
+	}
+
+	public void setCountry_id(int country_id) {
+		this.country_id = country_id;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public void setDevice_id(String device_id) {
+		this.device_id = device_id;
+	}
+
+	public void setDnt(int dnt) {
+		this.dnt = dnt;
+	}
+
+	public void setExchange_id(int exchange_id) {
+		this.exchange_id = exchange_id;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public void setHas_bidfloor(int has_bidfloor) {
+		this.has_bidfloor = has_bidfloor;
+	}
+
+	public void setHas_deviceid(int has_deviceid) {
+		this.has_deviceid = has_deviceid;
+	}
+
+	public void setHas_location(int has_location) {
+		this.has_location = has_location;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+
+	public void setImpression_type(int impression_type) {
+		this.impression_type = impression_type;
+	}
+
+	public void setMake_id(int make_id) {
+		this.make_id = make_id;
+	}
+
+	public void setMax_height(int max_height) {
+		this.max_height = max_height;
+	}
+
+	public void setMax_width(int max_width) {
+		this.max_width = max_width;
+	}
+
+	public void setMin_height(int min_height) {
+		this.min_height = min_height;
+	}
+
+	public void setMin_width(int min_width) {
+		this.min_width = min_width;
+	}
+
+	public void setOs(int os) {
+		this.os = os;
+	}
+
+	public void setOsv(int osv) {
+		this.osv = osv;
+	}
+
+	public void setPublisher_id(String publisher_id) {
+		this.publisher_id = publisher_id;
+	}
+
+	public void setPublisher_name(String publisher_name) {
+		this.publisher_name = publisher_name;
+	}
+
+	public void setSession_depth(int session_depth) {
+		this.session_depth = session_depth;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public void setTraffic_type(int traffic_type) {
+		this.traffic_type = traffic_type;
+	}
+
+	public void setVideo_height(int video_height) {
+		this.video_height = video_height;
+	}
+
+	public void setVideo_width(int video_width) {
+		this.video_width = video_width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
 }
--- gobblin-example/src/main/java/gobblin/applift/simpleconsumer/PSVToJSONConverter.java
+++ gobblin-example/src/main/java/gobblin/applift/simpleconsumer/PSVToJSONConverter.java
@@ -18,75 +18,123 @@ import gobblin.converter.DataConversionException;
 import gobblin.converter.SchemaConversionException;
 import gobblin.converter.SingleRecordIterable;
 
-public class PSVToJSONConverter extends Converter<Object, Object, String, String>{
-  private static final Logger LOG = LoggerFactory.getLogger(PSVToJSONConverter.class);
-  private static ObjectMapper mapper = new ObjectMapper();
-  
-  @Override
-  public Object convertSchema(Object inputSchema, WorkUnitState workUnit)
-      throws SchemaConversionException {
-    // TODO Auto-generated method stub
-    return null;
-  }
+public class PSVToJSONConverter extends Converter<Object, Object, String, String> {
+	private static final Logger LOG = LoggerFactory.getLogger(PSVToJSONConverter.class);
+	private static ObjectMapper mapper = new ObjectMapper();
 
-  @Override
-  public Iterable<String> convertRecord(Object outputSchema, String inputRecord,
-      WorkUnitState workUnit) throws DataConversionException { 
-    if (inputRecord.contains("LOGROTATE"))
-      return new SingleRecordIterable<String>(inputRecord);
-    String[] psvRecords = inputRecord.split("\n");
-    List<String> jsonRecords = new ArrayList<String>();
-    for (String psvRecord : psvRecords) {
-      try {
-        jsonRecords.add(psvToJson(psvRecord));
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        LOG.error("Erroneous Record : " + psvRecord);
-        e.printStackTrace();
-      }
-    }
-    return jsonRecords;
-  }
+	@Override
+	public Object convertSchema(Object inputSchema, WorkUnitState workUnit) throws SchemaConversionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-  public static String psvToJson(String psvRecord) throws IOException {
-    String[] fields = psvRecord.split("\\|");
-    ClickHouseRecord record = new ClickHouseRecord();
-    record.setExchangeId(fields[0]);
-    record.setPublisherId(fields[1]);
-    record.setBundleId(fields[3]);
-    record.setAppId(fields[4]);
-    record.setCountryId(fields[6]);
-    record.setOs(fields[10]);
-    record.setAge(Integer.parseInt(fields[15]));
-    record.setGender(fields[16]);
-    record.setDeviceId(getDeviceId(fields[22]));
-    record.setTimestamp(Long.parseLong(fields[23]));
-    record.setRequest(1);
-    record.setImp(0);
-    record.setClick(0);
-    record.setConversion(0);
-    record.setBid(0); 
-    return mapper.writeValueAsString(record);
-  }
-  
-  private static String getDeviceId(String deviceId) {
-    if (isDeviceId(deviceId)) {
-      return DigestUtils.sha1Hex(deviceId);
-    } else if (isValidSHA(deviceId)) {
-      return deviceId.toLowerCase();
-    }
-    return "";
-  }
+	@Override
+	public Iterable<String> convertRecord(Object outputSchema, String inputRecord, WorkUnitState workUnit)
+			throws DataConversionException {
+		if (inputRecord.contains("LOGROTATE"))
+			return new SingleRecordIterable<String>(inputRecord);
+		String[] psvRecords = inputRecord.split("\n");
+		List<String> jsonRecords = new ArrayList<String>();
+		for (String psvRecord : psvRecords) {
+			try {
+				jsonRecords.add(psvToJson(psvRecord));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				LOG.error("Erroneous Record : " + psvRecord);
+				e.printStackTrace();
+			}
+		}
+		return jsonRecords;
+	}
 
-  private static boolean isDeviceId(String id) {
-    String pattern = "(.*)(-)(.*)(-)(.*)(-)(.*)(-)(.*)";
-    Pattern r = Pattern.compile(pattern);
-    Matcher m = r.matcher(id);
-    return m.matches();
+	public static String psvToJson(String psvRecord) throws IOException {
+		String[] fields = psvRecord.split("\\|");
+		ClickHouseRecord record = new ClickHouseRecord();
+		if (!fields[0].isEmpty())
+			record.setExchange_id(Integer.parseInt(fields[0]));
+		else
+			record.setExchange_id(0);
+		record.setPublisher_id(fields[1]);
+		record.setPublisher_name(fields[2]);
+		record.setBundle_id(fields[3]);
+		record.setApp_id(fields[4]);
+		record.setApp_name(fields[5]);
+		if (!fields[6].isEmpty())
+			record.setCountry_id(Integer.parseInt(fields[6]));
+		if (!fields[7].isEmpty())
+			record.setCarrier_id(Integer.parseInt(fields[7]));
+		if (!fields[8].isEmpty())
+			record.setConnection_type(Integer.parseInt(fields[8]));
+		if (!fields[9].isEmpty())
+			record.setTraffic_type(Integer.parseInt(fields[9]));
+		if (!fields[10].isEmpty())
+			record.setOs(Integer.parseInt(fields[10]));
+		if (!fields[11].isEmpty())
+			record.setOsv(Integer.parseInt(fields[11]));
+		if (!fields[12].isEmpty())
+			record.setMake_id(Integer.parseInt(fields[12]));
+		if (!fields[13].isEmpty())
+			record.setHas_location(Integer.parseInt(fields[13]));
+		if (!fields[14].isEmpty())
+			record.setHas_deviceid(Integer.parseInt(fields[14]));
+		if (!fields[15].isEmpty())
+			record.setAge(Integer.parseInt(fields[15]));
+		if (!fields[16].isEmpty())
+			record.setGender(Integer.parseInt(fields[16]));
+		if (!fields[17].isEmpty())
+			record.setImpression_type(Integer.parseInt(fields[17]));
+		if (!fields[18].isEmpty())
+			record.setHas_bidfloor(Integer.parseInt(fields[18]));
+		if (!fields[19].isEmpty())
+			record.setHour(Integer.parseInt(fields[19]));
+		if (!fields[20].isEmpty())
+			record.setDate(fields[20]);
+		if (!fields[21].isEmpty())
+			record.setBid_floor(Double.parseDouble(fields[21]));
+		record.setDevice_id(getDeviceId(fields[22]));
+		if (!fields[23].isEmpty())
+			record.setTimestamp(Long.parseLong(fields[23]));
+		if (!fields[24].isEmpty())
+			record.setDnt(Integer.parseInt(fields[24]));
+		if (!fields[25].isEmpty())
+			record.setSession_depth(Integer.parseInt(fields[25]));
+		if (!fields[26].isEmpty())
+			record.setWidth(Integer.parseInt(fields[26]));
+		if (!fields[27].isEmpty())
+			record.setHeight(Integer.parseInt(fields[27]));
+		if (!fields[28].isEmpty())
+			record.setMin_width(Integer.parseInt(fields[28]));
+		if (!fields[29].isEmpty())
+			record.setMin_height(Integer.parseInt(fields[29]));
+		if (!fields[30].isEmpty())
+			record.setMax_width(Integer.parseInt(fields[30]));
+		if (!fields[31].isEmpty())
+			record.setMax_height(Integer.parseInt(fields[31]));
+		if (!fields[32].isEmpty())
+			record.setVideo_width(Integer.parseInt(fields[32]));
+		if (!fields[33].isEmpty())
+			record.setVideo_height(Integer.parseInt(fields[33]));
+		return mapper.writeValueAsString(record);
+	}
 
-  }
+	private static String getDeviceId(String deviceId) {
+		if (isDeviceId(deviceId)) {
+			return DigestUtils.sha1Hex(deviceId);
+		} else if (isValidSHA(deviceId)) {
+			return deviceId.toLowerCase();
+		}
+		return "";
+	}
 
-  private static boolean isValidSHA(String id) {
-    return (id.matches("[a-fA-F0-9]{40}"));
-  }
+	private static boolean isDeviceId(String id) {
+		String pattern = "(.*)(-)(.*)(-)(.*)(-)(.*)(-)(.*)";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(id);
+		return m.matches();
+
+	}
+
+	private static boolean isValidSHA(String id) {
+		return (id.matches("[a-fA-F0-9]{40}"));
+	}
 }
